5e55c7f65d5796fb29a8c0cab32b10be208c0ef3
vaibhav sharma <49065649+vaibhav950170@users.noreply.github.com>
2020-05-15 00:48:39 +0530

    Add files via upload

---
+++ animation name/app.js
@@ -0,0 +1,13 @@
+const express= require('express');
+const bodyParser = require('body-parser');
+const app=express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/index.html");
+})
+app.listen(process.env.PORT || 3000,function(req,res){
+  console.log("Server is Up");
+})
