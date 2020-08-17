7e82e40029359a4092d083f8ec8134bae08b0e12
vaibhav sharma <49065649+vaibhav950170@users.noreply.github.com>
2020-08-18 01:03:37 +0530

    Add files via upload

---
+++ portfolio-page/app.js
@@ -0,0 +1,11 @@
+const express=require('express');
+
+const app=express();
+app.use(express.static('public'));
+app.get("/",function(req,res){
+	res.sendFile(__dirname+"/index.html");
+});
+
+app.listen(process.env.PORT || 3000,function(){
+	console.log("Server is up");
+})
